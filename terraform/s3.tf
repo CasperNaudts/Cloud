e38700eb55388396db0b5bc5694ea11fa061b056
Casper Naudts <casper@edu-tech.be>
2019-11-25 14:26:07 +0100

    added script for s3

---
+++ terraform/s3.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  alias  = "west"
+  region = "us-west-1"
+}
+
+resource "aws_s3_bucket" "b" {
+  provider = aws.west
+  bucket = "project-cloud-snb-8"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  provider = aws.west
+  bucket = "${aws_s3_bucket.b.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "PublicReadGetObject",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::project-cloud-snb-8/*"
+    }
+  ]
+}
+POLICY
+}
