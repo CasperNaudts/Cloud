c351aa8e76dbf6285ca19da4df939b1d1571e299
11701177 <bergstromtommie@gmail.com>
2019-12-02 14:57:38 +0100

    Commitje su

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -10,10 +10,11 @@ resource "aws_security_group" "web" {
   }
 
   egress {
-    from_port       = 3306
-    to_port         = 3306
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
